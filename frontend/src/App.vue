a886e678c0c37dac7e7b408426fd9a7fc9e2819b
Yuvaraj K <22f2001731@ds.study.iitm.ac.in>
2026-07-07 13:56:22 +0530

    feat: Footer Component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <script setup>
 import AppHeader from './components/layout/AppHeader.vue';
-
+import AppFooter from './components/layout/AppFooter.vue';
 
 </script>
 
 <template>
 
   <AppHeader />
+
   <main>
     <router-view />
   </main>
+
+  <AppFooter />
 </template>
-
-<style scoped>
-
-</style>
---
+++ frontend/src/components/layout/AppFooter.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer class = "app-footer">
+        <span class = "footer-content">
+            Placement Portal            
+        </span>
+        <span class = "footer-content">
+            &copy; 2026. All Rights Reserved.           
+        </span>
+    </footer>
+</template>
+
+<style scoped>
+.app-footer {
+    position: sticky;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;      
+    gap: 4px;                  
+
+    height: var(--footer-height);
+
+    font-size: 16px;
+    color: var(--accent-primary-foreground);
+    border-top: 1px solid var(--surface-line);
+    background: var(--accent-primary-background);
+}
+
+.footer-content {
+    text-align: right;
+    margin: 0 16px;
+}
+</style>
