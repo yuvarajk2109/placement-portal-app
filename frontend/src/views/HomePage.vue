9e825a544415b45894d162d65a5dba88f643d0b5
Yuvaraj K <22f2001731@ds.study.iitm.ac.in>
2026-07-09 17:37:06 +0530

    fix: style update

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="mt-24">
-        <h1 class="page-title text-center">Welcome to the Placement Portal</h1>
+    <div class="mt-24 text-center">
+        <h1 class="page-title text-center">Welcome to the Placement Portal</h1>        
     </div>
 </template>
+
+<script setup>
+import { useDialogStore } from '@/stores/dialog'
+import { useNotificationStore } from '@/stores/notification'
+
+const dialog = useDialogStore()
+const notify = useNotificationStore()
+</script>
