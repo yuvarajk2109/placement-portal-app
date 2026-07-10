562fb03e3d22966d773f2150caf93e374f046e12
Yuvaraj K <22f2001731@ds.study.iitm.ac.in>
2026-07-10 20:41:38 +0530

    feat: Custom Multiselect Component

---
+++ frontend/src/components/ui/AppMultiSelect.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="multiselect-container">
+        <button type="button" v-for="option in options" :key="option[id]" class="btn is-small" :class="isSelected(option) ? 'is-primary' : 'is-secondary'" @click="toggleSelection(option)">{{ option[value] }}</button>
+    </div>
+</template>
+
+<style scoped>
+.multiselect-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+}
+</style>
+
+<script setup>
+const properties = defineProps({
+    modelValue: {
+        type: Array,
+        required: true
+    },
+    options: {
+        type: Array,
+        required: true
+    },
+    value: {
+        type: String,
+        required: true
+    },
+    id: {
+        type: String,
+        required: true
+    }
+});
+
+const emit = defineEmits(['update:modelValue', 'blur']);
+
+function isSelected(option) {
+    return properties.modelValue.includes(option[properties.id]);
+}
+
+function toggleSelection(option) {
+    const id = option[properties.id];
+    const index = properties.modelValue.indexOf(id);
+    const newIds = [...properties.modelValue];
+
+    if (index === -1) {
+        newIds.push(id);
+    } else {
+        newIds.splice(index, 1);
+    }
+
+    emit('update:modelValue', newIds);
+    emit('blur');
+}
+</script>
