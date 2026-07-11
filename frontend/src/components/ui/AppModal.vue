410d33dcefed59dc0f35ecfe1c4ca93531f32b7f
Yuvaraj K <22f2001731@ds.study.iitm.ac.in>
2026-07-11 13:33:48 +0530

    feat: Multiple modal sizes

--- frontend/src/components/ui/AppModal.vue
+++ frontend/src/components/ui/AppModal.vue
@@ -1,9 +1,9 @@
 <template>
 <Teleport to="body">
     <div v-if="modelValue" class="modal-overlay">
-        <div class="modal-content app-modal">
+        <div class="modal-content app-modal" :class="`is-${size}`">
             <div class="modal-header">
-                <h2 class="modal_title"><slot name="title">Modal</slot></h2>
+                <h2 class="modal-title"><slot name="title">Modal</slot></h2>
                 <button class="btn is-secondary is-icon-only" @click="close">
                     <i class="fas fa-times"></i>
                 </button>
@@ -21,10 +21,18 @@
 
 <style scoped>
 .app-modal {
-    min-width: 500px;
-    width: 800px;
+    width: 1000px;
     max-width: 1000px;
 }
+
+.app-modal.is-small {
+    width: 500px;
+}
+
+.app-modal.is-medium {
+    width: 750px;
+}
+
 .modal-body {
     margin-top: 16px;
     max-height: 60vh;
@@ -37,6 +45,12 @@ defineProps({
     modelValue: {
         type: Boolean,
         required: true
+    },
+    size: {
+        type: String,
+        required: false,
+        default: 'large',
+        validator: value => ['small', 'medium', 'large'].includes(value)
     }
 });
 
